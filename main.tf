935528e065d8798c2ec72f020e298083c73f6de5
Marcel Lupo <Marcel.Lupo@MottMac.com>
2021-03-12 18:04:49 +0000

    User testing

--- main.tf
+++ main.tf
@@ -117,12 +117,14 @@ resource "azurerm_role_definition" "terraform_role" {
 }
 
 resource "azurerm_role_assignment" "primary_rg_ra" {
+  depends_on         = [azuread_service_principal.terraform_app_sp]
   scope              = azurerm_resource_group.primary_rg.id
   role_definition_id = azurerm_role_definition.terraform_role.id
   principal_id       = azuread_service_principal.terraform_app_sp.id
 }
 
 resource "azurerm_role_assignment" "primary_sa_container_ra" {
+  depends_on           = [azuread_service_principal.terraform_app_sp]
   scope                = "${azurerm_resource_group.backend_rg.id}/providers/Microsoft.Storage/storageAccounts/${azurerm_storage_account.backend_sa.name}/blobServices/default/containers/${azurerm_storage_container.primary_sa_container.name}"
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azuread_service_principal.terraform_app_sp.id
@@ -143,6 +145,7 @@ resource "azurerm_key_vault_secret" "terraform_client_secret" {
 }
 
 resource "null_resource" "setup-log" {
+  depends_on         = [azurerm_key_vault.backend_kv]
   provisioner "local-exec" {
     command     = <<EOT
 $user = az account show | ConvertFrom-Json
