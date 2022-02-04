d03e2d199c4c3e20273a2c5d4606c3ddbea30e0f
Pwd9000 <magiconion_M@hotmail.com>
2022-02-04 16:49:28 +0000

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = "~> 1.0.0"
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = ">= 2.62"
+      version = "~> 2.62"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = ">= 1.5.0"
+      version = "~> 1.5.0"
     }
   }
 }
