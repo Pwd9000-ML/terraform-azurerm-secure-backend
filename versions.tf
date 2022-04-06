7e91fafded45c513a2b4a50bd0184ff25aa0e66b
M L <magiconion_M@hotmail.com>
2022-04-06 19:50:26 +0100

    change SPN

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.1"
+      version = "~> 2.99.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
