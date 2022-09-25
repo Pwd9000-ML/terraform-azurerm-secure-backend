8f549368698f99b5bbb4012ca823022390b10f26
Pwd9000 <pwd9000@hotmail.co.uk>
2022-09-25 15:11:38 +0100

    fix tf version

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 1.2.0"
+  required_version = "~> 1.3.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.23.0"
+      version = "~> 3.22.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
