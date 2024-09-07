b6f092143704ba18acc2e77f3da15ed4bf7a30c2
Pwd9000-ML <pwd9000@hotmail.co.uk>
2024-09-07 13:25:05 +0100

    chore: Update azurerm and azuread provider versions
    Update the versions of the azurerm and azuread providers in the versions.tf file. This commit updates the azurerm provider version to "~> 4.0" and the azuread provider version to "~> 2.0". These updates ensure that the latest versions of these providers are used in the Terraform configuration.

--- versions.tf
+++ versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0.0"
+      version = "~> 4.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.53.1"
+      version = "~> 2.0"
     }
   }
 }
